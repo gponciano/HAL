d1613a3137efe7a274626d191ddafe4e479a360c
gponciano <gab_bash@hotmail.com>
2025-02-24 19:10:33 +1100

    Adding MediatR

---
+++ Application/Activities/Queries/GetActivityList.cs
@@ -0,0 +1,23 @@
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using static Application.Activities.Queries.GetActivityList;
+
+
+namespace Application.Activities.Queries
+{
+    public class GetActivityList
+    {
+        public class Query : IRequest<List<Activity>> {}
+    
+
+    public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
+    {
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await context.Activities.ToListAsync(cancellationToken);
+        }
+    }
+    }
+}
