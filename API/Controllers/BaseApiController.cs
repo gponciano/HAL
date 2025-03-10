08e2026dd3365edd7375a0641ead53b89ce96de1
gponciano <gab_bash@hotmail.com>
2025-03-10 22:12:24 +1100

    adding commands

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,7 +10,14 @@ namespace API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
-    {
-        
+    {   // Using _ for private property as a naming convention
+        private IMediator? _mediator;
+        // Protected means this will be available for the class we declare in and classes that 
+        // derive from this class
+        protected IMediator Mediator 
+            => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+                ?? throw new InvalidOperationException("IMediator service is unavailable");
     }
+
+
 }
